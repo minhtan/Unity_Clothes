34e817f309a42d01241fbe048b66482b5d2c0076
Tan Minh <m.tan2007@gmail.com>
2016-10-18 02:21:32 +0700

    done testing

--- Assets/Capture.cs
+++ Assets/Capture.cs
@@ -2,37 +2,94 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class Capture : MonoBehaviour {
 	[SerializeField] string deviceName;
 	public RawImage webcamTexture;
 	public RawImage processedTexture;
-
+	Texture2D texture;
 	WebCamTexture wct;
 
 	// Use this for initialization
 	void Start () {
+		
+	}
+
+	void LoadFromResource(){
+		texture = Resources.Load<Texture2D> ("0");
+		var bits = texture.GetPixels ();
+		bits = ReverseXPixels (bits, texture.width, texture.height);
+		texture.SetPixels(bits);
+		texture.Apply();
+		var tempText = texture;
+		webcamTexture.texture = tempText;
+	}
+
+	void LoadFromResource2(){
+		texture = Resources.Load<Texture2D> ("test");
+		var bits = texture.GetPixels ();
+		bits = ReverseXPixels (bits, texture.width, texture.height);
+		texture.SetPixels(bits);
+		texture.Apply();
+
+		webcamTexture.texture = texture;
+	}
+		
+	bool StartWebcam(){
 		WebCamDevice[] devices = WebCamTexture.devices;
-		deviceName = devices[0].name;
-		wct = new WebCamTexture(deviceName, 1080, 720, 12);
-		webcamTexture.texture = wct;
-		wct.Play();
+		if (devices.Length > 0) {
+			deviceName = devices [0].name;
+			wct = new WebCamTexture (deviceName, 1280, 720, 12);
+			texture = new Texture2D(wct.width, wct.height);
+			webcamTexture.texture = texture;
+			wct.Play ();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void Update(){
+		if (wct != null && texture != null) {
+			var bits = wct.GetPixels ();
+			bits = ReverseXPixels (bits, wct.width, wct.height);
+
+			texture.SetPixels(bits);
+			texture.Apply();
+		}
 	}
 
 	void OnGUI() {      
-		if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
-			RemoveBg();
+		if (GUI.Button (new Rect (10, 10, 100, 30), "Start webcam"))
+			StartWebcam ();
+
+		if (GUI.Button (new Rect (10, 50, 100, 30), "rm bg"))
+			RemoveBgFromWebcamTexture ();
+
+		if (GUI.Button (new Rect (Screen.width / 2, 10, 100, 30), "Reload"))
+			SceneManager.LoadScene (0);
+
+		if (GUI.Button (new Rect (Screen.width - 110, 10, 100, 30), "Load res"))
+			LoadFromResource ();
+
+		if (GUI.Button (new Rect (Screen.width - 110, 50, 100, 30), "Load test"))
+			LoadFromResource2 ();
+
+		if (GUI.Button (new Rect (Screen.width - 110, 90, 100, 30), "rm bg"))
+			RemoveBgFromTexture ();
+
 
 	}
 
-	void RemoveBg()
+	void RemoveBgFromWebcamTexture()
 	{
-		Texture2D snap = new Texture2D(wct.width, wct.height);
+		Texture2D texture = new Texture2D(wct.width, wct.height);
 		var bits = wct.GetPixels ();
 
-		snap.SetPixels(bits);
-		snap.Apply();
-		SaveTextureToFile (snap, "0");
+		texture.SetPixels(bits);
+		texture.Apply();
+		SaveTextureToFile (texture, "0");
 
 		var colorToRemove = bits[0];
 		for (int i = 0; i < bits.Length; i++) {
@@ -41,15 +98,45 @@ public class Capture : MonoBehaviour {
 			}
 		}
 
-		snap.SetPixels(bits);
-		snap.Apply();
-		SaveTextureToFile (snap, "1");
+		texture.SetPixels(bits);
+		texture.Apply();
+		SaveTextureToFile (texture, "1");
 
-		processedTexture.texture = snap;
+		processedTexture.texture = texture;
+	}
+
+	void RemoveBgFromTexture()
+	{
+		var bits = texture.GetPixels ();
+
+		var colorToRemove = bits[0];
+		for (int i = 0; i < bits.Length; i++) {
+			if ( CheckColor(bits[i], colorToRemove) ) {
+				bits [i] = Color.clear;
+			}
+		}
+
+		texture.SetPixels(bits);
+		texture.Apply();
+		SaveTextureToFile (texture, "2");
+
+		processedTexture.texture = texture;
+	}
+
+	Color[] ReverseXPixels(Color[] bits, int w, int h){
+		Color[] ret = new Color[w * h];
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				ret[i * w + j] = bits[(w - j - 1) + (w * i)];
+			}
+		}
+
+		return ret;
 	}
 
 	bool CheckColor(Color color1, Color color2){
-		var threshold = 0.3f;
+		var threshold = 0.2f;
 		bool isRedGood = Mathf.Abs(color1.r - color2.r) <= threshold;
 		bool isGreenGood = Mathf.Abs(color1.g - color2.g) <= threshold;
 		bool isBlueGood = Mathf.Abs(color1.b - color2.b) <= threshold;
@@ -60,6 +147,6 @@ public class Capture : MonoBehaviour {
 	void SaveTextureToFile( Texture2D texture, string fileName)
 	{
 		Debug.Log (Application.dataPath);
-		System.IO.File.WriteAllBytes(Application.dataPath + "/"+fileName+".png", texture.EncodeToPNG()); 
+		System.IO.File.WriteAllBytes(Application.dataPath + "/Resources/"+fileName+".png", texture.EncodeToPNG()); 
 	}
 }
